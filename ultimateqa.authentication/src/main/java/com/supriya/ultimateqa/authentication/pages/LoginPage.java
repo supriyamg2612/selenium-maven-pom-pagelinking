24e9ef337a4070e3017c0a12bb91ef8193139cbb
supriya <supriya@ameyas-mbp.fritz.box>
2025-05-26 17:45:51 +0200

    Add login functionality with page object and test class

--- ultimateqa.authentication/src/main/java/com/supriya/ultimateqa/authentication/pages/LoginPage.java
+++ ultimateqa.authentication/src/main/java/com/supriya/ultimateqa/authentication/pages/LoginPage.java
@@ -7,6 +7,36 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	
+	WebDriver driver =null;
+	
+	
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id="user[email]")
+	WebElement emailField;
+	
+	@FindBy(id="user[password]")
+	WebElement passwordField;
+	
+	@FindBy(css="button[type='submit']")
+	WebElement signInButtonField;
+	
+	@FindBy(css = "a[href='/users/sign_up']")
+	WebElement createAccountField;
+
+  
+	
+	
+	public void loginToApplication(String uname, String pass) {
+		emailField.sendKeys(uname);
+		passwordField.sendKeys(pass);
+		signInButtonField.click();
+	
+		
 	
 
+}
 }
---
+++ ultimateqa.authentication/src/test/java/com/supriya/ultimateqa/authentication/tests/TestClass.java
@@ -0,0 +1,38 @@
+package com.supriya.ultimateqa.authentication.tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.supriya.ultimateqa.authentication.pages.LoginPage;
+
+
+public class TestClass {
+	
+WebDriver driver = null;
+	
+	@BeforeSuite
+	public void configuration() {
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+		 driver = new ChromeDriver();
+		driver.get("https://courses.ultimateqa.com/users/sign_in");
+	}
+	
+	@Test
+	public void testOne() {
+		LoginPage lp = new LoginPage(driver);
+		lp.loginToApplication("celinaredden123@gmail.com", "Celinaredden123");
+		WebDriverWait wait = new WebDriverWait(driver, 10); // 10 seconds timeout
+
+		wait.until(ExpectedConditions.urlToBe("https://courses.ultimateqa.com/collections"));
+
+		Assert.assertEquals(driver.getCurrentUrl(), "https://courses.ultimateqa.com/collections");
+	}
+
+}
