401c369dd85e718a6f5b917b6352d95a9265f73e
supriya <supriya@ameyas-mbp.fritz.box>
2025-05-26 15:53:11 +0200

    Initial commit: Set up Maven project with Page Object Model classes for ultimateqa page navigation

---
+++ ultimateqa.authentication/src/main/java/com/supriya/ultimateqa/authentication/pages/DashBoardPage.java
@@ -0,0 +1,5 @@
+package com.supriya.ultimateqa.authentication.pages;
+
+public class DashBoardPage {
+
+}
---
+++ ultimateqa.authentication/src/main/java/com/supriya/ultimateqa/authentication/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.supriya.ultimateqa.authentication.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	
+
+}
